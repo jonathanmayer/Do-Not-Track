508d9b54d375d39a3f8b3d8572ca96ffc3c84479
Jonathan Mayer <jmayer@stanford.edu>
2010-11-15 18:21:01 -0800

    adding jetpack module

---
+++ jetpack/donottrack.js
@@ -0,0 +1,30 @@
+/*
+donottrack.js
+Jonathan Mayer - jmayer@stanford.edu
+ 
+A Jetpack module that adds a Do Not Track header to all web requests.
+
+v0.01 - 11/14/10
+*/
+
+var DoNotTrackHeaderName = "X-Do-Not-Track";
+var DoNotTrackHeaderValue = "1";
+
+var {Cc, Ci} = require("chrome");
+var observerService = require("observer-service");
+
+var DNTCallback = function(subject, data)
+{
+	var httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
+	httpChannel.setRequestHeader(DoNotTrackHeaderName, DoNotTrackHeaderValue, false);
+};
+
+exports.register = function register()
+{
+	observerService.add("http-on-modify-request", DNTCallback);
+};
+
+exports.unregister = function unregister()
+{
+	observerService.remove("http-on-modify-request", DNTCallback);
+};
